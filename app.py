ea45310c7a21479f8a7e95b4a1237a4396ff0072
root <root@Ghali65.localdomain>
2024-12-17 15:00:26 +0100

    résumé fr et eng si non traduit

--- app.py
+++ app.py
@@ -1,47 +1,30 @@
 import streamlit as st
 import pandas as pd
 
-
+# Lire les CSV pour les DataFrames
 df_search = pd.read_csv('reco.csv')
-filtered_df = pd.read_csv('df_final.csv')
-
-def result(movie):
-    col1, col2 = st.columns(2)
-    pd.set_option('display.max_colwidth', None)
-    with col1:
-        df_search = pd.read_csv('reco.csv')
-        filtered_df = pd.read_csv('df_final.csv')
-        filtered_df = filtered_df[df_search['title_y'].str.contains(movie, case=False, na=False)]
-        st.title(filtered_df)
-
-    with col2:
-        st.header("A dog")
-        
-    return col1, col2
-
-
-
+df_final = pd.read_csv('df_final.csv')
 # Titre de l'application
 st.title('Barre de Recherche sur Streamlit')
 
 # Barre de recherche
-search_query = st.text_input('Entrez votre recherche ici')
-
-
+search_query = st.selectbox('Que voulez-vous regarder ?', options=df_search['title_y'])
 
 # Filtrer le DataFrame en fonction de la saisie utilisateur
 if search_query:
-    col1, col2 = st.columns(2)
-    pd.set_option('display.max_colwidth', None)
-    with col1:
-        filtered_df = filtered_df.apply([df_search['title_y'].str.contains(search_query, case=False, na=False)])
-        st.title(filtered_df)    
-    with col2:
-        st.header("A dog")
-else :
-    st.header('')
-
+    st.write('Résultats de la recherche :')
+    film_details = df_final[df_search['title_y'] == search_query]
+    film_title = film_details['title_y'].iloc[0] 
+    film_overview = film_details['résumé'].iloc[0]
+    film_poster = film_details['url_poster_path'].iloc[0]
+     # Afficher les résultats 
+    col1, col2 = st.columns([2, 1]) 
+    # Ajouter du contenu dans la première colonne 
+    with col1: 
+        st.write(f"**Titre du film :** {film_title}") 
+        st.write(f"**Résumé :** {film_overview}")
+    # Ajouter du contenu dans la deuxième colonne 
+    with col2: 
+        st.image(film_poster)
 # Afficher le DataFrame filtré
-st.write('Résultats de la recherche :')
-
 
