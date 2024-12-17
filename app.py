42a84116637ba9c5cb3a4b2f716a5d807f4c51c0
root <root@Ghali65.localdomain>
2024-12-17 17:28:34 +0100

    Premier jet site

--- app.py
+++ app.py
@@ -1,19 +1,27 @@
 import streamlit as st
 import pandas as pd
+from streamlit_image_select import image_select
 
 # Lire les CSV pour les DataFrames
 df_search = pd.read_csv('reco.csv')
 df_final = pd.read_csv('df_final.csv')
+df_final['Titres_voisins'] = df_final['Titres_voisins'].apply(pd.eval)
+df_final['directors'] = df_final['directors'].apply(pd.eval)
+df_final['production_companies_name'] = df_final['production_companies_name'].apply(pd.eval)
+df_final['genres_x'] = df_final['genres_x'].apply(pd.eval)
 # Titre de l'application
 st.title('Barre de Recherche sur Streamlit')
 
-# Barre de recherche
-search_query = st.selectbox('Que voulez-vous regarder ?', options=df_search['title_y'])
+# Ajouter une option vide comme première option 
 
-# Filtrer le DataFrame en fonction de la saisie utilisateur
-if search_query:
-    st.write('Résultats de la recherche :')
-    film_details = df_final[df_search['title_y'] == search_query]
+options = [''] + df_search['title_y'].tolist()
+
+# Barre de recherche
+search_query = st.selectbox('Que voulez-vous regarder ?', options=options)
+
+def resultat_recherche(text):
+    st.subheader('Résultats de la recherche :')
+    film_details = df_final[df_search['title_y'] == text]
     film_title = film_details['title_y'].iloc[0] 
     film_overview = film_details['résumé'].iloc[0]
     film_poster = film_details['url_poster_path'].iloc[0]
@@ -26,5 +34,22 @@ if search_query:
     # Ajouter du contenu dans la deuxième colonne 
     with col2: 
         st.image(film_poster)
-# Afficher le DataFrame filtré
 
+
+
+
+# Filtrer le DataFrame en fonction de la saisie utilisateur
+if search_query:
+    resultat_recherche(search_query)
+    st.subheader('Nos recommandations :')
+    film_recommandation = df_final[df_search['title_y'] == search_query]
+    liste_reco =  film_recommandation['Titres_voisins'].iloc[0]
+    liste_poster=[]
+    for movie in range(10) :
+        film_details = df_final[df_search['title_y'] == liste_reco[movie]]
+        film_poster = film_details['url_poster_path'].iloc[0]
+        liste_poster.append(film_poster)
+    carroussel = image_select("Sélectionnez un film", liste_poster, use_container_width=True)
+    # Récupérer les détails du film sélectionné 
+    selected_film = df_final[df_final['url_poster_path'] == carroussel]['title_y'].values[0] 
+    resultat_recherche(selected_film)
